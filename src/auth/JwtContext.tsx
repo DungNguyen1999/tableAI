e9d3ec27c899f06fd7799a6fdba5e17b49d8dcae
DungNguyen1999 <dungnguyen2342@gmail.com>
2023-12-07 16:20:15 +0700

    hiden user

--- src/auth/JwtContext.tsx
+++ src/auth/JwtContext.tsx
@@ -143,28 +143,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       });
       console.log('response', response);
-      const { token  } = response.data;
+      const { token } = response.data;
       setSession(token);
       const user = {
-         id: "8864c717-587d-472a-929a-8e5f298024da-0",
-        displayName: "Jaydon Frankie",
-        email: "demo@minimals.cc",
-        password: "demo1234",
-        photoURL: "https://api-dev-minimal-v4.vercel.app/assets/images/avatars/avatar_default.jpg",
-        phoneNumber: "+40 777666555",
-        country: "United States",
-        address: "90210 Broadway Blvd",
-        state: "California",
-        city: "San Francisco",
-        zipCode: "94116",
-        about: "Praesent turpis. Phasellus viverra nulla ut metus varius laoreet. Phasellus tempus.",
-        role: "admin",
-        isPublic: true
-      }
+        id: '8864c717-587d-472a-929a-8e5f298024da-0',
+        // displayName: 'Jaydon Frankie',
+        // email: "demo@minimals.cc",
+        // password: 'demo1234',
+        photoURL: 'https://api-dev-minimal-v4.vercel.app/assets/images/avatars/avatar_default.jpg',
+        phoneNumber: '+40 777666555',
+        country: 'United States',
+        address: '90210 Broadway Blvd',
+        state: 'California',
+        city: 'San Francisco',
+        zipCode: '94116',
+        about:
+          'Praesent turpis. Phasellus viverra nulla ut metus varius laoreet. Phasellus tempus.',
+        role: 'admin',
+        isPublic: true,
+      };
       dispatch({
         type: Types.LOGIN,
         payload: {
-          user ,
+          user,
         },
       });
     } catch (err) {
